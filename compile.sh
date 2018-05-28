0863ae662c909fad5a01cce63dd3f19d70a7c440
Lab User <labuser@libr318-02r.its.carleton.edu>
2018-05-28 17:57:40 -0500

    further automation

--- compile.sh
+++ compile.sh
@@ -1,4 +1,13 @@
+# compile a latex resume and rename based on date, which
+# will hopefully keep a history of resumes for me.
 pdflatex -jobname=resume_$(date +"%d%b%Y") cv.tex
+# copy the most recent version of my resume to relics
+cp cv.tex relics/resume_$(date +"%d%b%Y").tex
+# clean up
 rm most_recent.png
-#cp resume_$(date +"%d%b%Y").pdf most_recent.pdf
+# convert the most recent pdf to 
 sips -s format png --out "most_recent.png" "resume_$(date +"%d%b%Y").pdf"
+# more clean up
+cp resume_$(date +"%d%b%Y").pdf most_recent.pdf
+mv resume_$(date +"%d%b%Y").pdf relics/
+rm *.log *.aux *.out

6b7441f74b8e08feed3a2f32cc406a3e35f0ec29
Lab User <labuser@libr318-02r.its.carleton.edu>
2018-05-28 17:48:50 -0500

    working on embedding images

--- compile.sh
+++ compile.sh
@@ -1,3 +1,4 @@
 pdflatex -jobname=resume_$(date +"%d%b%Y") cv.tex
-rm most_recent.pdf
-cp resume_$(date +"%d%b%Y").pdf most_recent.pdf
+rm most_recent.png
+#cp resume_$(date +"%d%b%Y").pdf most_recent.pdf
+sips -s format png --out "most_recent.png" "resume_$(date +"%d%b%Y").pdf"
